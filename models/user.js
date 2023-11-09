1d1bab1aea9600895e573f9eb0406f38b2bafe4f
Alan Pasoń <alan.pason132@gmail.com>
2023-11-09 21:13:58 +0100

    Add: Auth

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: {type: String, required: true},
-  password: {type: String, required: true},
-  admin: {type: Boolean, default: false},
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  admin: { type: Boolean, default: false },
 });
 
 const User = mongoose.model("User", UserSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const verifyToken = require("../verifyToken");
 
 const post_controller = require("../controllers/postController");
 const tag_controller = require("../controllers/tagController");
@@ -26,16 +30,105 @@ router.get("/", function (req, res, next) {
 
 // Posts
 
+// register
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  // Perform input validation here
+
+  if (!password) {
+    return res.status(400).json({ error: "Password is missing" });
+  }
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Create and save the user
+  const user = new User({ username, password: hashedPassword });
+  await user.save();
+
+  res.json({ message: "User registered successfully" });
+});
+
+// Login
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(401).json({ message: "Authentication failed" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Authentiaction failed" });
+  }
+
+  const token = jwt.sign(
+    { userId: user._id, username: user.username },
+    "secretkey",
+    { expiresIn: "1h" }
+  );
+
+  res.json({ message: "Authentication successful" });
+});
+
+//login to cms
+
+router.post("/cms/login", async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+
+  const user = await User.findOne({ username });
+  console.log(user);
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ message: "Authentication failed no user" });
+  }
+
+  if (!user.admin) {
+    return res
+      .status(401)
+      .json({ message: "You need admin permission" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Authentiaction failed" });
+  }
+
+  const token = jwt.sign(
+    { userId: user._id, username: user.username },
+    "secretkey",
+    { expiresIn: "1h" }
+  );
+
+  res.json({ message: "Authentication successful", token });
+});
+
 router.get("/posts/:id", post_controller.get_single_post);
 
 router.post(
   "/posts/:id",
+  verifyToken,
   upload.single("photoUrl"),
   post_controller.update_single_post
 );
 
 router.post(
   "/posts",
+  verifyToken,
   upload.single("photoUrl"),
   post_controller.create_post
 );
